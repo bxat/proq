fe5a89551c8c3a2e8dc4c3a0e569823c759471e0
h <K>
2022-12-04 16:01:10 +0100

    ad

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const cookieParser = require("cookie-parser");
+const axios = require('axios')
 const { requireAuth } = require("./middleware/authMiddleware");
 
 require("dotenv").config();
@@ -12,6 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 // app.use(cookieParser())
+app.get('/', (req, res) => {
+  res.json('Got you')
+})
 
 app.post("/protected", requireAuth);
 app.listen(process.env.PORT, () => {
@@ -20,9 +24,15 @@ app.listen(process.env.PORT, () => {
 
 app.use(authRoutes);
 app.use("/admin", adminRoutes);
-setInterval(() => {
-  console.log('a')
 
-}, 845000);
+
+setInterval(() => {
+  axios.get('https://p-roq.onrender.com/').then(resp => {
+
+    console.log(resp.data);
+  }).catch((err) => {
+    console.log({ err })
+  });
+}, 780000);
 
 //PASSWORD minimum length
